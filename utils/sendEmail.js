9456eb470528cd4df2939f61e6f62b2837649119
Liza <156740104+lizajulaha2427@users.noreply.github.com>
2025-12-21 18:21:14 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,15 +2,16 @@ import nodemailer from "nodemailer";
 
 export const sendOTPEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.sendgrid.net",
+    port: 587,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "apikey", // literally the string "apikey"
+      pass: process.env.SENDGRID_API_KEY, // your SendGrid API key
     },
   });
 
   await transporter.sendMail({
-    from: `"PrepRoadmap" <${process.env.EMAIL_USER}>`,
+    from: '"PrepRoadmap" <no-reply@prepsphere.com>',
     to: email,
     subject: "Your OTP Code",
     html: `<h3>Your OTP is: <b>${otp}</b></h3><p>Valid for 10 minutes</p>`,
