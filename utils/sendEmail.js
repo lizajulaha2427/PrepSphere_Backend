0f6a4b326b332feb3279f70f5d7ceb82a614458d
Jiya Mehta <jiya0704.be23@chitkara.edu.in>
2026-07-12 18:37:33 +0530

    fix

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,16 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false, // false for port 587
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
